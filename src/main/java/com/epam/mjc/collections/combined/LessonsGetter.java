79eebb1b86c76d65e6dc5fc7182c4b270d76e29f
Matvey Stetcenko <matveyvs1987@gmail.com>
2023-01-21 01:37:14 +0400

    Problems solved

--- src/main/java/com/epam/mjc/collections/combined/LessonsGetter.java
+++ src/main/java/com/epam/mjc/collections/combined/LessonsGetter.java
@@ -1,10 +1,14 @@
 package com.epam.mjc.collections.combined;
 
+import java.util.*;
 import java.util.List;
-import java.util.Map;
-import java.util.Set;
 
 public class LessonsGetter {
     public Set<String> getLessons(Map<String, List<String>> timetable) {
+        Set<String> result = new TreeSet<>();
+        for (Map.Entry<String, List<String>> entry : timetable.entrySet()) {
+            result.addAll(entry.getValue());
+        }
+        return result;
     }
 }
--- src/main/java/com/epam/mjc/collections/combined/MapFromKeysCreator.java
+++ src/main/java/com/epam/mjc/collections/combined/MapFromKeysCreator.java
@@ -1,9 +1,17 @@
 package com.epam.mjc.collections.combined;
 
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
 
 public class MapFromKeysCreator {
     public Map<Integer, Set<String>> createMap(Map<String, Integer> sourceMap) {
+        Map<Integer, Set<String>> result = new HashMap<>();
+        for (Map.Entry<String,Integer> entry : sourceMap.entrySet()){
+            if (result.containsKey(entry.getKey().length())){
+                result.get(entry.getKey().length()).add(entry.getKey());
+            } else {
+                result.put(entry.getKey().length(), new HashSet<>(Collections.singleton(entry.getKey())));
+            }
+        }
+        return result;
     }
 }
